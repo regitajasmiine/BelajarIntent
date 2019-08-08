f32ac0c6c2984bd7ca319ba1abb6afafffdfd994
regitajasmiine <regitajasmine17@gmail.com>
2019-08-08 13:19:38 +0700

    belajar

---
+++ app/src/main/java/com/example/belajarintent/IntentActivity.java
@@ -0,0 +1,18 @@
+package com.example.belajarintent;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.belajarintent.R;
+
+public class IntentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+    }
+
+}
